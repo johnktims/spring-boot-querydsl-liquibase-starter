7311a5bb5d5818f6fa8b96526be354e5feee01c5
John Tims <john.k.tims@gmail.com>
2015-03-30 19:32:39 -0400

    Add date field to Person

--- src/main/java/com/example/ApplicationRunner.java
+++ src/main/java/com/example/ApplicationRunner.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.util.Date;
+import java.util.HashSet;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +13,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashSet;
+import com.mysema.query.jpa.impl.JPAQuery;
 
 @SpringBootApplication
 public class ApplicationRunner implements CommandLineRunner {
@@ -20,20 +26,28 @@ public class ApplicationRunner implements CommandLineRunner {
     @Autowired
     AddressRepository addressRepository;
 
+    @PersistenceContext
+    EntityManager em;
+
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        Person person1 = personRepository.save(new Person("person1", 20, new HashSet<Address>()));
-        Person person2 = personRepository.save(new Person("person2", 30, new HashSet<Address>()));
+        Person person1 = personRepository.save(new Person("person1", 20, new Date(), new HashSet<Address>()));
+        Person person2 = personRepository.save(new Person("person2", 30, new Date(), new HashSet<Address>()));
 
         addressRepository.save(new Address(person1, "some street 1", 12345));
         addressRepository.save(new Address(person2, "some street 2", 54321));
 
         for (Person person : personRepository.findAll(QPerson.person.name.eq("person1"))) {
             logger.info("{}: {}", person.getName(), person.getAge());
+        }
+
+        JPAQuery query = new JPAQuery(em);
+        for (Person person : query.from(QPerson.person).list(QPerson.person)) {
+            logger.info("{}: {}", person.getName(), person.getBirthday());
         }
     }
 }
--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -1,26 +1,30 @@
 package com.example;
 
-import javax.persistence.*;
-import java.io.Serializable;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.*;
 
 @Entity
-public class Person implements Serializable {
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "NAME"))
+public class Person implements java.io.Serializable {
 
     private Integer id;
     private String name;
     private Integer age;
+    private Date birthday;
     private Set<Address> addresses = new HashSet<Address>(0);
 
     public Person() {
     }
 
-    public Person(String name, Integer age, Set<Address> addresses) {
+    public Person(String name, Integer age, Date birthday, Set<Address> addresses) {
         this.name = name;
         this.age = age;
+        this.birthday = birthday;
         this.addresses = addresses;
     }
 
@@ -30,33 +34,47 @@ public class Person implements Serializable {
     public Integer getId() {
         return this.id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
 
     @Column(name = "NAME", unique = true, length = 50)
     public String getName() {
         return this.name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
 
     @Column(name = "AGE")
     public Integer getAge() {
         return this.age;
     }
+
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "BIRTHDAY", length = 23)
+    public Date getBirthday() {
+        return this.birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
     }
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "person")
     public Set<Address> getAddresses() {
         return this.addresses;
     }
+
     public void setAddresses(Set<Address> addresses) {
         this.addresses = addresses;
     }
 }
-
-
